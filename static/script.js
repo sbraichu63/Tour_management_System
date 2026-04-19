46d1008dad59fe85e66aff4e181cabb047c28715
Suryansh Barnawal <suryanshbarnawal890@gmail.com>
2026-04-19 14:48:09 +0530

    changed the overall services section

--- static/script.js
+++ static/script.js
@@ -9,9 +9,8 @@ menu.onclick = () => {
 window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
-    loginForm.classList.remove('active');
+    // loginForm.classList.remove('active');   // Remove or fix this line
 };
-
 
 var swiper = new Swiper(".home-slider", {
     loop: true,
